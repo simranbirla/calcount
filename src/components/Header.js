a3b6aa6c4fa052e14c4b9f1aa9302a1c1271bcad
simranbirla <62006675+simranbirla@users.noreply.github.com>
2020-12-11 15:53:38 +0530

    header responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,24 +2,33 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../styling/Header.css";
 const Header = () => {
+  const closeHeader = () => {
+    document
+      .getElementsByClassName("header")[0]
+      .setAttribute("display", "none");
+  };
   return (
-    <div className="header">
-      <Link to="/frontpage" className="header-elements">
-        Home <span></span>
-      </Link>
-      <Link to="/" className="header-elements">
-        Count Cals <span></span>
-      </Link>
-      <Link to="/food" className="header-elements">
-        Meal<span></span>
-      </Link>
-      <Link to="/exercise" className="header-elements">
-        Exercise <span></span>
-      </Link>
-      <Link to="/recipes" className="header-elements">
-        Recipes <span></span>
-      </Link>
-    </div>
+    <>
+      <input type="checkbox" id="check" />
+      <label htmlFor="check" className="label" label="true"></label>
+      <div className="header">
+        <Link to="/frontpage" className="header-elements">
+          Home <span></span>
+        </Link>
+        <Link to="/" className="header-elements">
+          Count Cals <span></span>
+        </Link>
+        <Link to="/food" className="header-elements">
+          Meal<span></span>
+        </Link>
+        <Link to="/exercise" className="header-elements">
+          Exercise <span></span>
+        </Link>
+        <Link to="/recipes" className="header-elements">
+          Recipes <span></span>
+        </Link>
+      </div>
+    </>
   );
 };
 
